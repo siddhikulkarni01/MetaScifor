f1e1f9be16719cf6de97eb8ee26b960d59ea1660
siddhikulkarni <125177669+siddhikulkarni01@users.noreply.github.com>
2025-12-27 13:11:18 +0530

    Delete assignment 1 directory

--- assignment 1/asg1.py
+++
@@ -1,8 +0,0 @@
-"""Arvind just started learning python and he is very interested to know how he
- can print his name on the screen using python. Write a program to print Arvind 
- on the screen. Print your name and age as well using a python print statement. 
-"""
-
-print("Arvind")
-print("My name is siddhi")
-print("Age 23")
